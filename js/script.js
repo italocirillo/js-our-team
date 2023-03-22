da1d3620092593f2a6489040f0400afa3f0e5325
italocirillo <italocirillo@gmail.com>
2023-03-22 22:43:58 +0100

    ESERCIZIO COMPLETO + I BONUS + STILIZZATO

--- js/script.js
+++ js/script.js
@@ -32,18 +32,29 @@ const team = [
     }
 ];
 // ELEMENTI HTML
-const listaMembriTeam=document.getElementById("lista-membri-team");
-
-
+const riga = document.querySelector(".row");
+let colonne ="";
 for(let i=0;i<team.length;i++){
-    listaMembriTeam.innerHTML += `
-    <li><h4>Membro ${i+1}</h4>
-        <ul class="informazioni-membro"></ul>
-    </li>`;
-    const informazioniMembro=document.querySelector(`li:nth-child(${i+1}) .informazioni-membro`);
-    for (const key in team[i]) {
-        informazioniMembro.innerHTML += `<li >${key}:${team[i][key]}</li>`;
-        console.log(`${key}:${team[i][key]}`);
-    }
-    console.log("--------------------------");
+    // listaMembriTeam.innerHTML += `
+    // <li><h4>Membro ${i+1}</h4>
+    //     <ul class="informazioni-membro"></ul>
+    // </li>`;
+    // const informazioniMembro=document.querySelector(`li:nth-child(${i+1}) .informazioni-membro`);
+    // for (const key in team[i]) {
+    //     informazioniMembro.innerHTML += `<li >${key}:${team[i][key]}</li>`;
+    //     console.log(`${key}:${team[i][key]}`);
+    // }
+    // console.log("--------------------------");
+    colonne += `
+    <div class="col">
+        <div class="card">
+            <img src="img/${team[i].immagine}" class="card-img-top" alt="${team[i].immagine}">
+            <div class="card-body">
+                <h5 class="card-title nome">${team[i].nome}</h5>
+                <p class="card-text ruolo">${team[i].ruolo}</p>
+            </div>
+        </div>
+    </div>
+    `;
+    riga.innerHTML = colonne;
 }
