d46d9b4b997bb35cdf3d6cf471143a128cdf43d1
italocirillo <italocirillo@gmail.com>
2023-03-22 19:11:11 +0100

    MILESTONE 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("javascript collegato");
+const team = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "founder & CEO",
+        immagine: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        immagine: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        immagine: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        immagine: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "barbara-ramos-graphic-designer.jpg"
+    },
+];
